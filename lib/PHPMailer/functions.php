4e20fbea060aafc0d3c1c62ca4862c6f91b7df81
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-01-09 22:45:24 +0700

    version 2023

--- lib/PHPMailer/functions.php
+++ lib/PHPMailer/functions.php
@@ -1,25 +1,27 @@
 <?php
 // $user = "Cskhkabistore@gmail.com";
 // $password = "hoangthao123";
-$user = "hotrokhachhang@kabistore.com.vn";
-$password = "sieubaomat96308520@";
 
-function sendMail($user, $password, $title, $content, $nTo, $mTo, $diachicc = '')
+
+function sendMail($title, $content, $nTo, $mTo, $diachicc = '')
 {
+	$user = "toptopapphostmail@gmail.com";
+	$password = "bqfhhroqxjbigvvl";
+
 	$nFrom = 'Kabistore.com.vn';
-	$mFrom = $user;	//dia chi email cua ban 
-	$mPass = $password;		//mat khau email cua ban
-	$mail             = new PHPMailer();
-	$body             = $content;
+	$mFrom = $user; //dia chi email cua ban 
+	$mPass = $password; //mat khau email cua ban
+	$mail = new PHPMailer();
+	$body = $content;
 	$mail->IsSMTP();
-	$mail->CharSet 	= "utf-8";
-	$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-	$mail->SMTPAuth   = true;                  	// enable SMTP authentication
-	$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
-	$mail->Host       = "mail.kabistore.com.vn"; //smtp.gmail.com
-	$mail->Port       = 465;
-	$mail->Username   = $mFrom;  // GMAIL username
-	$mail->Password   = $mPass;           	 // GMAIL password
+	$mail->CharSet = "utf-8";
+	$mail->SMTPDebug = 0; // enables SMTP debug information (for testing)
+	$mail->SMTPAuth = true; // enable SMTP authentication
+	$mail->SMTPSecure = "tls"; // sets the prefix to the servier
+	$mail->Host = "smtp.gmail.com"; //smtp.gmail.com
+	$mail->Port = 587; // 465
+	$mail->Username = $mFrom; // GMAIL username
+	$mail->Password = $mPass; // GMAIL password
 	$mail->SetFrom($mFrom, $nFrom);
 	//chuyen chuoi thanh mang
 	$ccmail = explode(',', $diachicc);
@@ -29,11 +31,11 @@ function sendMail($user, $password, $title, $content, $nTo, $mTo, $diachicc = ''
 			$mail->AddCC($v);
 		}
 	}
-	$mail->Subject    = $title;
+	$mail->Subject = $title;
 	$mail->MsgHTML($body);
 	$address = $mTo;
 	$mail->AddAddress($address, $nTo);
-	$mail->AddReplyTo('hotrokhachhang@kabistore.com.vn', 'https://Kabistore.com.vn');
+	$mail->AddReplyTo('toptopapphostmail@gmail.com', 'http://Kabistore.com.vn');
 	if (!$mail->Send()) {
 		return 0;
 	} else {
@@ -44,19 +46,19 @@ function sendMail($user, $password, $title, $content, $nTo, $mTo, $diachicc = ''
 function sendMailAttachment($user, $password, $title, $content, $nTo, $mTo, $diachicc = '', $file, $filename)
 {
 	$nFrom = 'Kabistore.tk';
-	$mFrom = $user;	//dia chi email cua ban 
-	$mPass = $password;		//mat khau email cua ban
-	$mail             = new PHPMailer();
-	$body             = $content;
+	$mFrom = $user; //dia chi email cua ban 
+	$mPass = $password; //mat khau email cua ban
+	$mail = new PHPMailer();
+	$body = $content;
 	$mail->IsSMTP();
-	$mail->CharSet 	= "utf-8";
-	$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-	$mail->SMTPAuth   = true;                  	// enable SMTP authentication
-	$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
-	$mail->Host       = "smtp.gmail.com";
-	$mail->Port       = 465;
-	$mail->Username   = $mFrom;  // GMAIL username
-	$mail->Password   = $mPass;           	 // GMAIL password
+	$mail->CharSet = "utf-8";
+	$mail->SMTPDebug = 0; // enables SMTP debug information (for testing)
+	$mail->SMTPAuth = true; // enable SMTP authentication
+	$mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 465;
+	$mail->Username = $mFrom; // GMAIL username
+	$mail->Password = $mPass; // GMAIL password
 	$mail->SetFrom($mFrom, $nFrom);
 	//chuyen chuoi thanh mang
 	$ccmail = explode(',', $diachicc);
@@ -66,7 +68,7 @@ function sendMailAttachment($user, $password, $title, $content, $nTo, $mTo, $dia
 			$mail->AddCC($v);
 		}
 	}
-	$mail->Subject    = $title;
+	$mail->Subject = $title;
 	$mail->MsgHTML($body);
 	$address = $mTo;
 	$mail->AddAddress($address, $nTo);
